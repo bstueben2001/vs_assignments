49c2e01af51439dca7a10dfc34e87b95f8168888
Brendon Stueben <bstueben2001@gmail.com>
2025-01-21 21:56:35 -0700

    uploading progress on DOMOlympics project

---
+++ dom-olympics-start/index.js
@@ -0,0 +1,21 @@
+
+var headerText = document.getElementById("header")
+
+headerText.innerHTML += "<header>JavaScript Made This!!</header>"
+headerText.innerHTML += "<span>Brendon</span><header>wrote the JavaScript</header>"
+    // headerText.style.color = "orange"
+
+
+
+
+
+    var eraseButton = document.getElementById("#clear-button")
+
+    eraseButton.document.addEventListener("click",clear)
+
+    var messages = document.getElementsByClassName("#messages")
+
+
+    function clear() {
+        messages.parentNode.removeChild(messages);
+    }
