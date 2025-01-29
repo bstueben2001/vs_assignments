e0c86cbe0b7f84b13c914a204dca6cea7f714753
Brendon Stueben <bstueben2001@gmail.com>
2025-01-28 23:04:27 -0700

    adding files for moms shopping list project

--- Notes/practice.js
+++ Notes/practice.js
@@ -1,14 +1,43 @@
+// const form = document["my-form"]
+
+// form.addEventListener("submit", function(event){
+//     event.preventDefault()
+    
+//     const firstName = form.firstName.value
+//     const lastName = form.lastName.value
+//     form.firstName.value = ""
+//     form.lastName.value = ""
+
+//     const h1 = document.createElement('h1')
+//     h1.textContent = firstName + " " + lastName
+//     document.getElementsByTagName("body")[0].append(h1)
+// })
+
 const form = document["my-form"]
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", (event) => {
     event.preventDefault()
-    
-    const firstName = form.firstName.value
-    const lastName = form.lastName.value
-    form.firstName.value = ""
-    form.lastName.value = ""
 
-    const h1 = document.createElement('h1')
-    h1.textContent = firstName + " " + lastName
-    document.getElementsByTagName("body")[0].append(h1)
+    // console.log(form.entertain)
+
+    // const checkedInputs = []
+
+    // for(let i = 0; i < form.entertain,length; i++){
+    //     if(form.entertain[i].checked){
+    //         checkedInputs.push(form.entertain[i].value)
+    //     }
+
+    // }
+
+    // console.log(checkedInputs)
+
+console.log(form.city.value)
+
+
+
+
+
+
+
+
 })
