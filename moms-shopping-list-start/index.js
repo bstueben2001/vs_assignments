53f85988ce50fcc895412786eae83bfddcf7915d
Brendon Stueben <bstueben2001@gmail.com>
2025-02-05 23:38:02 -0700

    savng work

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,50 +1,46 @@
 const form = document.addItem
 
-form.addEventListener("submit",function(event){
-    event.preventDefault()
-    const itemName = form.title.value
-    form.title.value = ""
+const anchor = document.getElementById("list")
 
-    var newItem = [
-        itemName,
-        "edit",
-        "X"
-    ]
+    form.addEventListener("submit",function(event){
+        event.preventDefault()
+        const itemName = form.title.value
+        form.title.value = ""
 
-    var currentLi = document.getElementById("list")
+        let newLi = document.createElement("li")
 
-    var newLi = document.createElement("li")
-        newLi.textContent = newItem[0]
+            let item = document.createElement("span")
+                item.innerHTML = itemName
 
-    var editButton = document.createElement("button")
-        editButton.innerHTML = newItem[1]
-            //add styles for 'b1' class
+            let editButton = document.createElement("button")
+                editButton.innerHTML = "edit"
+                    //add styles for 'b1' class
+                    editButton.addEventListener("click",function(){
+                        console.log("This listener works")
+                        //input field???
+                        
 
-            editButton.addEventListener("click",function(){
-                console.log("This listener works")
-                //input field???
-                const itemName = form.title.value
-                newLi.textContent = itemName
-            })
-    
-    var clearButton = document.createElement("button")
-        clearButton.innerHTML = newItem[2]
-            clearButton.addEventListener("click",function(){
-                console.log("So does this one")
 
-            })
+                    })
 
-    for(let i = 0; i < newItem.length; i++)
-        currentLi.append(newLi)
-        currentLi.append(editButton)
-        currentLi.append(clearButton)
+            let clearButton = document.createElement("button")
+                clearButton.innerHTML = "X"
+                clearButton.addEventListener("click",function(){
+                    console.log("it worked")
+                    newLi.remove()
+                    })
 
+        newLi.append(item)
+        newLi.append(editButton)
+        newLi.append(clearButton)
+
+        anchor.appendChild(newLi)
+    })
+
+const oldButtons = document.querySelectorAll(".button2")
+oldButtons.forEach(button => {
+    button.addEventListener("click", function(){
+        const li = button.closest("li")
+        li.remove()
+    })
 })
-
-clearButton.addEventListener("click",function(){
-    for(let i = 0; i < newItem.length; i++)
-        newLi.remove(newLi)
-        newLi.remove(editButton)
-        newLi.remove(clearButton)
-})
-
