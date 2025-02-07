b68af31747007dbde85b1027df8165a889a2e5d0
Brendon Stueben <bstueben2001@gmail.com>
2025-02-06 22:44:15 -0700

    upload mid level assessment to git repo

---
+++ mid-level-assessment/assess.js
@@ -0,0 +1,6 @@
+
+let changeButton = document.getElementById("changeButton")
+    changeButton.addEventListener("click",function(){
+        document.getElementById("heading").textContent = "Welcome to my website!"
+        document.getElementById("paragraph").textContent = "This is a sample paragraph."
+    })
