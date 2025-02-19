11078d39b8d312d5bb050e145a43fd47f6aa942b
Brendon Stueben <bstueben2001@gmail.com>
2025-02-19 01:19:18 -0700

    adding string methods exercise files

---
+++ string_methods_exercise/index.js
@@ -0,0 +1,12 @@
+capped("hello")
+
+function capped(str) {
+    const upperStr = str.toUpperCase()
+    console.log(upperStr)
+    // return upperStr
+    // let newVer = upperStr =+ capped
+    // console.log(newVer)
+    // return newVer
+}
+
+
