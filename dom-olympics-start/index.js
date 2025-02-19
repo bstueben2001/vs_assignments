26b70c8fa27baa4def94524215c7d8d402cb608a
Brendon Stueben <bstueben2001@gmail.com>
2025-02-19 00:27:30 -0700

    Reworking DOM Olympics assignment while helping peer

--- dom-olympics-start/index.js
+++ dom-olympics-start/index.js
@@ -9,12 +9,39 @@ headerText.innerHTML += "<span>Brendon</span><header>wrote the JavaScript</heade
 
 
 
-    var eraseButton = document.getElementById("#clear-button")
+    // var eraseButton = document.getElementById("#clear-button")
 
-    eraseButton.document.addEventListener("click",clear)
+    // eraseButton.document.addEventListener("click",clear)
 
-    var messages = document.getElementsByClassName("#messages")
+    // var messages = document.getElementsByClassName("#messages")
 
-    function clear() {
-        messages.parentNode.removeChild(messages);
-    }
+    // function clear() {
+    //     messages.parentNode.removeChild(messages);
+    // }
+
+    const themeOption = document.getElementById("theme-drop-down")
+    themeOption.addEventListener("change",function(event){
+        const selectedOption = event.target.value
+        const left = document.querySelectorAll("message left")
+        const right = document.querySelectorAll("message right")
+        if(selectedOption === "theme-one"){
+            console.log("blue-brown, huzzah!")
+            left.forEach(function(element){
+                element.style.color = "blue"
+            })
+            right.forEach(function(element){
+                element.style.color = "brown"
+            })
+        } else if(selectedOption === "theme-two"){
+            console.log("red-black, hooray!")
+            left.forEach(function(element){
+                element.style.color = "red"
+            })
+            right.forEach(function(element){
+                element.style.color = "black"
+            })
+        }
+    })
+
+
+
