049c6fc39d621da334ec25d97f9eb8780441af54
Brendon Stueben <bstueben2001@gmail.com>
2025-02-23 18:57:48 -0700

    Finalized string methods exercise

--- string_methods_exercise/index.js
+++ string_methods_exercise/index.js
@@ -1,12 +1,42 @@
-capped("hello")
+// 1.
+// function altered(str) {
+//     console.log(str)
+//     let capped = str.toUpperCase()
+//     console.log(capped)
+//     let combined = capped + str
+//     console.log(combined)
+// }
 
-function capped(str) {
-    const upperStr = str.toUpperCase()
-    console.log(upperStr)
-    // return upperStr
-    // let newVer = upperStr =+ capped
-    // console.log(newVer)
-    // return newVer
+// altered("ham")
+
+
+// 2.
+// function findMidIndex(str2) {
+//     const str3 = Math.floor(str2.length / 2)
+//     console.log(str3)
+// }
+
+// findMidIndex("hello world")
+
+
+// 3.
+// function retFirstHalf(str4) {
+//     // const str5 = Math.floor(str4.length / 2)
+//     const str5 = str4.slice(0, Math.floor(str4.length / 2))
+//     console.log(str5)
+// }
+
+// retFirstHalf("Hello World")
+
+
+// 4.
+function halfCapped(str) {
+    const str2 = str.slice(0, Math.floor(str.length / 2))
+    // console.log(str2)
+    const str3 = str2.toUpperCase(str2)
+    // console.log(str3)
+    const str4 = str3 + str.slice(Math.floor(str.length / 2))
+    console.log(str4)
 }
 
-
+halfCapped("hello world")
