3f64ccb84f9052a7af380ea88ad73ed21bce69e5
Brendon Stueben <bstueben2001@gmail.com>
2025-03-25 00:14:29 -0600

    shifting business time project from laptop to pc, and cloning bugs on a plane

---
+++ business_time_project/index.js
@@ -0,0 +1,98 @@
+const img1Effect = document.getElementById("img1")
+
+
+img1Effect.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const img2Effect = document.getElementById("img2")
+
+
+img2Effect.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const img3Effect = document.getElementById("img3")
+
+
+img3Effect.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const img4Effect = document.getElementById("img4")
+
+
+img4Effect.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const img5Effect = document.getElementById("img5")
+
+
+img5Effect.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const homeButton = document.getElementById("home")
+
+
+homeButton.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const productsButton = document.getElementById("products")
+
+
+productsButton.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const aboutButton = document.getElementById("about")
+
+
+aboutButton.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
+
+const contactButton = document.getElementById("contact")
+
+
+contactButton.addEventListener("click", () =>
+{
+    console.log("This listener is working")
+
+
+})
+
