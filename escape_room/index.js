4c7bf88ff767c8c02f14c9d129a13ca0490cd556
Brendon Stueben <bstueben2001@gmail.com>
2025-04-02 22:18:19 -0600

    uploading business time, npm, and corrected escape room

--- escape_room/index.js
+++ escape_room/index.js
@@ -1,33 +1,35 @@
-function escape(){
-    const keySearch = document.getElementById("keyButton")
-    keySearch.addEventListener("click", () => console.log("You died. Don't put your hand in mysterious holes."))
-    // if(putHandInHole = true){
-    //  console.log("You died. Don't put your hand in mysterious holes.")
-    // } else {
-    //     console.log("The door is open. You're free!")
-    // }
-}
+const readline = require("readline-sync");
 
-const mysHole = document.getElementById("mysButton")
-mysHole.addEventListener("click", function() {
-    console.log("You died. Don't put your hand in mysterious holes. That means you Charlie.")
-})
+let hasKey = false; //starting game without item in inventory
 
-let keyInPossession = false
-const keySearch = document.getElementById("keyButton")
-keySearch.addEventListener("click", function() {
-    keyInPossession = true
-    console.log("You found a key!")
-})
+console.log("You're locked in a room with a mysterious hole, and a locked door. Just to clarify, the door is locked. Do you put your hand in the mysterious hole or do you search for a key?")
 
-const openDoor = document.getElementById("openButton")
-openDoor.addEventListener("click", function() {
-    if(keyInPossession){
-        console.log("The key unlocks the door. You're free! Good job not putting your hand in mysterious holes!")
+while(true) {
+    console.log("What do you do?")
+    console.log("1. Look for a key.")
+    console.log("2. Put hand in mysterious hole.")
+    console.log("3. Try the door.")
+
+    const choice = readline.question("Enter your choice: ")
+
+    if (choice === "1") {
+        if (!hasKey) {
+            hasKey = true;
+            console.log("You found a key.")
+        } else {
+            console.log("You already have the key.")
+        }
+    } else if (choice === "2") {
+        console.log("You died. Don't put your hand in mysterious holes. That means you Charlie.");
+        break;
+    } else if (choice === "3") {
+        if (hasKey) {
+            console.log("The key unlocks the door. You're free! Good job not putting your hand in mysterious holes!")
+            break;
+        } else {
+            console.log("It's locked...")
+        }
     } else {
-        console.log("It's locked...")
+        console.log("Wrong. Try option 1, 2, or 3.")
     }
-})
-
-
-
+}
